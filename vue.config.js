bfb1e7ac2f95414612ceb76da8acc2c358e85301
João Reis <joao.reis21@gmail.com>
2023-07-05 00:10:55 -0300

    add webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      //
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        minSize: 9000,
+        maxSize: 140000,
+      },
+    },
+  },
+  devServer: {
+    allowedHosts: ["all"],
+  },
 });
